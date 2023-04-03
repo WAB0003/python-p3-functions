adf556c0d9d0be5c1a9dc9525e415c05de4f76ba
Bill Brown <bill.a.brown90@gmail.com>
2023-04-03 10:03:37 -0700

    learning basic functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
+#!function in javacript:
+# function myFunction(parameters) {
+#     console.log("running myFunction")
+#     return param+1
+# }
+
+# const myFunctionReturnValue = myFunction(1)
+# console.log(myFunctionReturnValue)
+
+#! Writing same function in python it's called a method
+# def my_function(parameters):
+#     print("running my_function")
+#     return parameters + 1
+
+
+
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number/2
